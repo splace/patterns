2cd82ba6152a981881d324fbf710eb12a6fdee61
simon <psiplace@netscape.net>
2016-08-02 20:05:50 +0100

    comment

--- doc.go
+++ doc.go
@@ -1,9 +1,9 @@
 /*
-Package patterns generates, stores, downloads and manipulates abstract patterns, when imported it can then be used with specific real-world quantities.
+Package patterns generates, and manipulates abstract patterns, no colour specified, when imported it can then be used with specific image types.
 
 Definition of 'pattern'
 
-A varying property as it depends, uniquely, on two parameters.
+A varying property depending, uniquely, on two parameters.
 
 The controlling parameters are generally unbounded.
 
@@ -11,13 +11,13 @@ Fundamental Types
 
 x :- 'parameters' designed to be used as if unbounded (+ve and -ve), with unitX near the centre of its precision range.
 
-y :- 'property' can have a value between limits, +unitY and -unitY.
+y :- 'property' can have a value interpolated between limits of +-unitY.
 
 Interfaces 
 
 Pattern :- as method at(x,x)y which returns a 'y' value from two 'x' value parameters.
 
-LimitedPattern :- a Pattern with a MaxX() method returning the the 'x' value outside which the Pattern can be assumed to return zero, it effectively has an size.
+LimitedPattern :- a Pattern with a MaxX() method returning the 'x' value range outside which the Pattern can be assumed to return zero, it effectively has an size.
 
 */
 package patterns
