e4e369282b230b592ea6ec4c7fa88c1d77a59663
simon <psiplace@netscape.net>
2016-08-05 22:07:25 +0100

    comment

--- image_test.go
+++ image_test.go
@@ -33,7 +33,7 @@ func TestImageBox(t *testing.T) {
 	//m := &composable{image.NewPaletted(image.Rect(0, -150, 800, 150), palette.WebSafe)}
 	//m.draw(WebSafePalettedImage{ds})
 	//jpeg.Encode(file, m, nil)
-	pd:=NewDepiction(NewBox(198,1,Filling{unitY}), 400, 400, color.Opaque, color.Transparent)
+	pd:=NewDepiction(NewBox(199,1,Filling{unitY}), 400, 400, color.Opaque, color.Transparent)
 	fmt.Println(pd)
 	png.Encode(file, Plan9PalettedImage{pd})
 }
@@ -68,13 +68,4 @@ func (i *composable) drawOverOffset(isrc image.Image, pt image.Point) {
 	draw.Draw(i, i.Bounds(), isrc, isrc.Bounds().Min.Add(pt), draw.Over)
 }
 
-/*  Hal3 Fri Aug 5 22:05:56 BST 2016 go version go1.5.1 linux/amd64
-=== RUN   TestImageSquare
---- PASS: TestImageSquare (0.00s)
-=== RUN   TestImageBox
-{{[{   197.00 {X}} {{   199.00 {X}}}]    199.00} {{-200 -200} {200 200}} 1 {65535} {0}}
---- PASS: TestImageBox (0.07s)
-PASS
-ok  	_/home/simon/Dropbox/github/working/patterns	0.097s
-Fri Aug 5 22:05:58 BST 2016 */
 
