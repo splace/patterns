48175588461fa1a51a354de492d75864db044610
simon <psiplace@netscape.net>
2016-08-17 22:30:27 +0100

    temp remove brush test

--- brush_test.go
+++ brush_test.go
@@ -7,14 +7,5 @@ import (
 //"testing"
 )
 
-func ExampleBrushLine() {
-	b := Brush{Width: 2, In: unitY, Relative: true}
-	Output(b.Line(0, 0, 10, 10))
-	/* Output:
-	   0.00%                                  X
-	   0.00%                                  X
-	   0.00%                                  X
-	*/
-}
 
 
