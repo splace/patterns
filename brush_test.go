d21b840019f6379599f729c053a90b261f863944
simon <psiplace@netscape.net>
2016-08-17 22:35:01 +0100

    add

--- brush_test.go
+++ brush_test.go
@@ -7,5 +7,14 @@ import (
 //"testing"
 )
 
+func ExampleBrushLine() {
+	b := Brush{Width: 2, In: unitY, Relative: true}
+	Output(b.Line(0, 0, 10, 10))
+	/* Output:
+	   0.00%                                  X
+	   0.00%                                  X
+	   0.00%                                  X
+	*/
+}
 
 
---
+++ expose.go
@@ -0,0 +1,38 @@
+package patterns
+
+// convert to internal x representation, 1 -> UnitX
+func X(d interface{}) x {
+	return MultiplyX(d, unitX)
+}
+
+// multiply anything by an x quantity
+func MultiplyX(m interface{}, d x) x {
+	switch mt := m.(type) {
+	case int:
+		return d * x(mt)
+	case uint:
+		return d * x(mt)
+	case int8:
+		return d * x(mt)
+	case uint8:
+		return d * x(mt)
+	case int16:
+		return d * x(mt)
+	case uint16:
+		return d * x(mt)
+	case int32:
+		return d * x(mt)
+	case uint32:
+		return d * x(mt)
+	case int64:
+		return d * x(mt)
+	case uint64:
+		return d * x(mt)
+	case float32:
+		return x(float32(d)*mt + .5)
+	case float64:
+		return x(float64(d)*mt + .5)
+	default:
+		return d
+	}
+}
