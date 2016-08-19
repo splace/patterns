a125330e9174980e2fca9388fdc42d3c7e0d0919
simon <psiplace@netscape.net>
2016-08-19 23:45:19 +0100

    EOF

--- svg.go
+++ svg.go
@@ -6,29 +6,29 @@ import "errors"
 
 //import "fmt"
 
-const bufLen = 1
+const bufLen = 100
 
 
-type errParse struct {
+type errParsingReader struct {
 	error
 	Source      io.Reader
 }
 
-func (e errParse) Parsing() io.Reader {
+func (e errParsingReader) Parsing() io.Reader {
 	return e.Source
 }
 
 
 func Polygon(p Brush, cs string) (Pattern, error) {
-	sReader := strings.NewReader(cs)
-	fReader := float32ListReader{Reader: sReader, buf: make([]byte, bufLen)}
+	reader := strings.NewReader(cs)
+	fReader := float32ListReader{Reader: reader, buf: make([]byte, bufLen)}
 	coordsBuf := make([]float32, 10)
 	c, err := fReader.ReadFloat32s(coordsBuf)
-	if err != nil {
-		return nil,errParse{err,fReader}
+	if err != io.EOF {
+		return nil,errParsingReader{err,fReader}
 	}
 	if c%2 != 0 {
-		return nil,errParse{errors.New("Coordinate count."),fReader}
+		return nil,errParsingReader{errors.New("Coordinate count."),reader}
 	}
 	coords := make([][2]x, c/2)
 	for n := 0; n < c; n += 2 {
@@ -36,6 +36,17 @@ func Polygon(p Brush, cs string) (Pattern, error) {
 	}
 	return p.Polygon(coords), nil
 }
+
+type StageCompleted uint8
+
+const (
+	None StageCompleted = iota
+	wholeSign
+	whole
+	Fraction
+	ExponentSign
+	Exponent
+)
 
 type float32ListReader struct {
 	io.Reader
@@ -82,29 +93,24 @@ func (this *float32ListReader) ReadFloat32s(fs []float32) (c int, err error) {
 			return 1e10 * power10(n-10)
 		}
 	}
-	var val func() float32
-	// assemble parts into complete value
-	val = func() float32 {
-		return (float32(this.partial) + float32(this.partialFraction)/power10(uint(this.partialFractionDigits))) * power10(this.partialExponent)
-	}
 	var value func() float32
 	value = func() float32 {
-		return (float32(this.partial) + float32(this.partialFraction)/power10(uint(this.partialFractionDigits))) / power10(this.partialExponent)
+		return (float32(this.partial) + float32(this.partialFraction)/power10(uint(this.partialFractionDigits))) 
 	}
 	// put value into array
 	var setVal func()
 	setVal = func() {
 		if this.neg {
 			if this.negExponent {
-				fs[c] = -value()
+				fs[c] = -value()/ power10(this.partialExponent)
 			} else {
-				fs[c] = -val()
+				fs[c] = -value()* power10(this.partialExponent)
 			}
 		} else {
 			if this.negExponent {
-				fs[c] = value()
+				fs[c] = value()/ power10(this.partialExponent)
 			} else {
-				fs[c] = val()
+				fs[c] = value()* power10(this.partialExponent)
 			}
 		}
 		//fmt.Println(c,":",fs[c])
@@ -113,12 +119,12 @@ func (this *float32ListReader) ReadFloat32s(fs []float32) (c int, err error) {
 	var n int
 	var b []byte
 	for err == nil {
-		if this.unBuf == nil || len(this.unBuf) == 0 { // use any unread first
-			n, err = this.Reader.Read(this.buf)
-			b = this.buf
-		} else {
+		if len(this.unBuf) != 0 { // use any unread first
 			n = len(this.unBuf)
 			b = this.unBuf
+		} else {
+			n, err = this.Reader.Read(this.buf)
+			b = this.buf
 		}
 		if n == 0 {
 			break
@@ -164,7 +170,7 @@ func (this *float32ListReader) ReadFloat32s(fs []float32) (c int, err error) {
 				this.charFound = false
 			case 'e', 'E':
 				if !this.charFound {
-					return c, errParse{errors.New("Can't parse as number."),this.Reader}
+					return c, errParsingReader{errors.New("Can't parse as number."),this.Reader}
 				} // cant have e wihout something before
 				this.wholeFound = true
 				this.fractionFound = true
@@ -186,11 +192,11 @@ func (this *float32ListReader) ReadFloat32s(fs []float32) (c int, err error) {
 					}
 				} else {
 					if !this.inSeparator {
-						return c, errParse{errors.New("Separator without value."),this.Reader}
+						return c, errParsingReader{errors.New("Separator without value."),this.Reader}
 					}
 				}
 				this.charFound = false
-			case ' ', '\n', '\r', '\t': // accept these as separators, but multiple occurances onyl count once. // two tabs only one separator in an svg path
+			case ' ', '\n', '\r', '\t': // accept these as separators, but multiple occurances only count once. // two tabs only one separator in an svg path
 				if !this.inSeparator {
 					this.inSeparator = true
 					if this.wholeFound || this.charFound {
@@ -220,17 +226,17 @@ func (this *float32ListReader) ReadFloat32s(fs []float32) (c int, err error) {
 						}
 					}
 				} else {
-					return c, errParse{errors.New("Can't parse as number."),this.Reader}
+					return c, errParsingReader{errors.New("Can't parse as numbers."),this.Reader}
 
 				}
 				if this.wholeFound && !this.fractionFound {
-					return c, errParse{errors.New("Can't parse as number."),this.Reader}
+					return c, errParsingReader{errors.New("Can't parse as numbers."),this.Reader}
 
 				}
 			case '+':
 				this.inSeparator = false
 				if this.charFound || this.wholeFound && !this.fractionFound {
-					return c, errParse{errors.New("Can't parse as number."),this.Reader}
+					return c, errParsingReader{errors.New("Can't parse as numbers."),this.Reader}
 
 				}
 
@@ -252,7 +258,7 @@ func (this *float32ListReader) ReadFloat32s(fs []float32) (c int, err error) {
 						return c, err
 					}
 				}
-				return c, errParse{errors.New("Uninterpretable character found."),this.Reader}
+				return c, errParsingReader{errors.New("Uninterpretable character found."),this.Reader}
 			}
 		}
 		this.unBuf = this.unBuf[0:0]
@@ -262,7 +268,6 @@ func (this *float32ListReader) ReadFloat32s(fs []float32) (c int, err error) {
 	}
 	return c, err
 }
-
 
 
 
